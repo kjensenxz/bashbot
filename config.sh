3832872ad5effdeb691cfa23bce05023f603a2cf
Kenneth B. Jensen <kenneth@jensen.cf>
2016-05-29 19:32:11 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,10 +1,10 @@
 # BashBot config
 # server
-network="irc.rizon.sexy"
-nickname="the8ballmeme"
+network="irc.rizon.net"
+nickname="bashbot"
 port="6697"
 ssl=yes
 # space sep list of chans
 channels="#chan1 #chan2 #chanx"
-infile="/tmp/in-8ballbot"
-outfile="/tmp/out-8ballbot"
+infile="/tmp/in-bashbot"
+outfile="/tmp/out-bashbot"
